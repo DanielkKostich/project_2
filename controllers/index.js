ceb65ce985ab8d7814642fb45da408443510805d
Garrett <grt002@outlook.com>
2023-05-11 20:47:48 -0500

    Adding functionality with passport.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
+var passport = require('passport');
+var session = require('express-session');
+
+var SQLiteStore = require('connect-sqlite3')(session);
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(passport.authenticate('session'));
+app.use('/', indexRouter);
+app.use('/', authRouter);
 
 module.exports = router;

5053ca1403c881b15cd258ceeee623421c5950e3
Lauren Wollaston <laurenwollaston@outlook.com>
2023-05-16 18:46:39 -0500

    aaa

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 const customerRoutes = require('./controllers/api/customerRoutes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Set up middleware
 app.use(express.urlencoded({ extended: true }));

503e370e5f521e7b1e829a4d652ad6fda4ad656a
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-05-15 20:48:57 -0500

    updated customer routes to read all existing customers; added 'cors' to dependencies because I'm trying to work on a bug with the login page; changed 'custemps' to 'reviews' for customer reviews of employees

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const customerRoutes = require('./customerRoutes');
 
-router.use('/users', userRoutes);
+router.use('/customers', customerRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const cors = require('cors');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -29,6 +30,7 @@ const sess = {
   };
   
   app.use(session(sess));
+  app.use(cors({ origin: '*', methods: 'PUT'}));
   
   // Inform Express.js on which template engine to use
   app.engine('handlebars', hbs.engine);

b0194ad0c8a9b2dfdd45f1dfd8ce39777ccfb8d0
Lauren Wollaston <laurenwollaston@outlook.com>
2023-05-17 13:11:08 -0500

    Changed the styling on the reviews page. Looks cleaner. Successfully accesses rating and displays stars based on that.

--- seeds/review-seeds.js
+++ seeds/review-seeds.js
@@ -4,6 +4,7 @@ const reviewData = [
   {
     rating: 5,
     review: 'He didn\'t leave a hair uncut!',
+    sentence: 'Master barber!',
     customer_cusid: 1,
     employee_empid: 2,
     appointment_id:1,
@@ -11,6 +12,7 @@ const reviewData = [
   {
     rating: 1,
     review: 'He cut my whole head off!',
+    sentence: 'Too sharp!',
     customer_cusid: 2,
     employee_empid: 1,
     appointment_id:2,
@@ -18,6 +20,7 @@ const reviewData = [
   {
     rating: 3,
     review: 'OK I guess',
+    sentence: 'Alright',
     customer_cusid: 3,
     employee_empid: 2,
     appointment_id:3,
@@ -25,6 +28,7 @@ const reviewData = [
   {
     rating: 5,
     review: 'Came back and told him to glue it all back on just so he could cut it off again.',
+    sentence: 'Better the second time!',
     customer_cusid: 1,
     employee_empid: 2,
     appointment_id:4,
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,28 @@
+const Handlebars = require('handlebars');
+
 module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
   format_amount: (amount) => {
-    // format large numbers with commas
+    // Format large numbers with commas
     return parseInt(amount).toLocaleString();
   },
+  starRating: function (rating) {
+    let stars = '';
+
+    // Generate the checked stars
+    for (let i = 0; i < rating; i++) {
+      stars += '<span class="fa fa-star checked"></span>';
+    }
+
+    // Generate the unchecked stars
+    for (let i = rating; i < 5; i++) {
+      stars += '<span class="fa fa-star"></span>';
+    }
+
+    return new Handlebars.SafeString(stars);
+  },
 };
+Handlebars.registerHelper('starRating', module.exports.starRating);

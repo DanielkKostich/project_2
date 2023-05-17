323059fce9f3c05e833ef3c5177ecf134c754cc8
Lauren Wollaston <laurenwollaston@outlook.com>
2023-05-16 19:32:24 -0500

    restored functionality

--- server.js
+++ server.js
@@ -1,28 +1,58 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const customerRoutes = require('./controllers/api/customerRoutes');
+const exphbs = require('express-handlebars');
+const cors = require('cors');
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Set up middleware
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'your-secret-key',
   resave: false,
   saveUninitialized: false
 }));
+app.use(routes);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+app.use(cors({ origin: '*', methods: 'PUT'}));
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 // Set up view engine
 const handlebars = require('express-handlebars');
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
 
 // Set up Passport.js authentication strategy
 passport.use(new LocalStrategy(
@@ -57,48 +87,43 @@ passport.deserializeUser(async (id, done) => {
 });
 
 // Set up routes
-app.get('/', (req, res) => {
-  res.render('home');
-});
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
 
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}));
 
-app.get('/register', (req, res) => {
-  res.render('register');
-});
+// app.post('/login', passport.authenticate('local', {
+//   successRedirect: '/',
+//   failureRedirect: '/login',
+//   failureFlash: true
+// }));
 
-app.post('/register', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const existingUser = await User.findOne({ username: username });
-    if (existingUser) {
-      return res.render('register', { message: 'Username already taken.' });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username: username, email: email, password: hashedPassword });
-    await user.save();
-    req.login(user, (error) => {
-      if (error) {
-        return next(error);
-      }
-      return res.redirect('/');
-    });
-  } catch (error) {
-    return res.render('register', { message: 'Error creating user.' });
-  }
-});
+// app.get('/register', (req, res) => {
+//   res.render('register');
+// });
+
+// app.post('/register', async (req, res) => {
+//   try {
+//     const { username, email, password } = req.body;
+//     const existingUser = await User.findOne({ username: username });
+//     if (existingUser) {
+//       return res.render('register', { message: 'Username already taken.' });
+//     }
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     const user = new User({ username: username, email: email, password: hashedPassword });
+//     await user.save();
+//     req.login(user, (error) => {
+//       if (error) {
+//         return next(error);
+//       }
+//       return res.redirect('/');
+//     });
+//   } catch (error) {
+//     return res.render('register', { message: 'Error creating user.' });
+//   }
+// });
 
 // API routes
-app.use('/api/customer', customerRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
-});
+  
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(port, () => console.log('Now listening at http://localhost:3001/'));
+  });
--- utils/auth.js
+++ utils/auth.js
@@ -2,23 +2,23 @@ var express = require('express');
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
 var bcrypt = require('bcrypt');
-var db = require('../db');
+// var db = require('../db');
 var router = express.Router();
 
-passport.use(new LocalStrategy(function verify(username, password, cb) {
-  db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, user) {
-    if (err) { return cb(err); }
-    if (!user) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+// passport.use(new LocalStrategy(function verify(username, password, cb) {
+//   db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, user) {
+//     if (err) { return cb(err); }
+//     if (!user) { return cb(null, false, { message: 'Incorrect username or password.' }); }
 
-    bcrypt.pbkdf2(password, user.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-      if (err) { return cb(err); }
-      if (!bcrypt.timingSafeEqual(user.hashed_password, hashedPassword)) {
-        return cb(null, false, { message: 'Incorrect username or password.' });
-      }
-      return cb(null, user);
-    });
-  });
-}));
+//     bcrypt.pbkdf2(password, user.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+//       if (err) { return cb(err); }
+//       if (!bcrypt.timingSafeEqual(user.hashed_password, hashedPassword)) {
+//         return cb(null, false, { message: 'Incorrect username or password.' });
+//       }
+//       return cb(null, user);
+//     });
+//   });
+// }));
 
 router.get('/login', function(req, res, next) {
   res.render('login');
@@ -56,26 +56,26 @@ router.get('/signup', function(req, res, next) {
   res.render('signup');
 });
 
-router.post('/signup', function(req, res, next) {
-  var salt = bcrypt.randomBytes(16);
-  bcrypt.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-    if (err) { return next(err); }
-    db.run('INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
-      req.body.username,
-      hashedPassword,
-      salt
-    ], function(err) {
-      if (err) { return next(err); }
-      var user = {
-        id: this.lastID,
-        username: req.body.username
-      };
-      req.login(user, function(err) {
-        if (err) { return next(err); }
-        res.redirect('/');
-      });
-    });
-  });
-});
+// router.post('/signup', function(req, res, next) {
+//   var salt = bcrypt.randomBytes(16);
+//   bcrypt.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+//     if (err) { return next(err); }
+//     db.run('INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+//       req.body.username,
+//       hashedPassword,
+//       salt
+//     ], function(err) {
+//       if (err) { return next(err); }
+//       var user = {
+//         id: this.lastID,
+//         username: req.body.username
+//       };
+//       req.login(user, function(err) {
+//         if (err) { return next(err); }
+//         res.redirect('/');
+//       });
+//     });
+//   });
+// });
 
 module.exports = router;

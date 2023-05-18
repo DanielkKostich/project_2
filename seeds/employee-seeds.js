cad563bff9f388c772ac1db30ac29b880e1ebc69
DanielkKostich <117228151+DanielkKostich@users.noreply.github.com>
2023-05-18 13:25:03 -0500

    added more customers, employees and a link to employee schedule to render only if logged in

--- seeds/employee-seeds.js
+++ seeds/employee-seeds.js
@@ -26,6 +26,21 @@ const employeeData = [
     email: 'shopbanana@msn.com',
     password: 'peachespeaches',
   },
+  {
+    name: 'Tom Petty',
+    email: 'tpet1324@yahoo.com',
+    password: 'thIs2356',
+  },
+  {
+    name: 'April Betts',
+    email: 'abett6489@gmail.com',
+    password: 'Hello48!',
+  },
+  {
+    name: 'Breanne Hollister',
+    email: 'bholla@yahoo.com',
+    password: 'Goodday',
+  },
 ];
 
 const seedEmployees = () => Employee.bulkCreate(employeeData);
